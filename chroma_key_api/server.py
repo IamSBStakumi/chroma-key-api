f1a4353106df58c3de2b58d201d690afcce3ad1e
kuri884 <krc.kuribayashi@gmail.com>
2024-07-04 16:28:41 +0900

    create post method

--- chroma_key_api/server.py
+++ chroma_key_api/server.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 server = FastAPI()
 
+# リクエストボディの型定義
+class Param(BaseModel):
+    contrast_adjustment_value: float
+    chroma_key_color: list[int]
+    chroma_key_threshold: int
+    noise_removal_iterations: int
+
 @server.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@server.post("/create")
+async def create_movie(param: Param):
+    return {"message", f'{param.contrast_adjustment_value},{param.chroma_key_color},{param.chroma_key_threshold},{param.noise_removal_iterations}'}
 
 if __name__ == "__main__":
     uvicorn.run(server, host="0.0.0.0", port=8080, log_level="debug")
