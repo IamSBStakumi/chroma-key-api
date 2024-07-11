04ef6bcd850a8b80f999c366d3611c0e941a5730
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2024-07-11 17:15:46 +0900

    exception_handlerを追加

--- main/server.py
+++ main/server.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Request, UploadFile, File
+from fastapi import FastAPI, Request, UploadFile, File, status
 import cv2
 import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 from variables import *
@@ -34,14 +36,19 @@ async def root():
     return {"message": "Hello World"}
 
 @server.post("/compose")
-async def create_movie(image: UploadFile=File(...), video: UploadFile=File(...)):
-    print(image.filename)
+async def compose_movie(image: UploadFile=File(...), video: UploadFile=File(...)):
+    print("heeee", image.filename)
     # 画像ファイルの内容を読み取る
     image_content = await image.read()
     # 動画ファイルの内容を読み取る
     video_content = await video.read()
 
-    return {"message", f'{image.filename}{video.filename}'}
+    return JSONResponse(content={"message": "hello"}, status_code=200)
+
+@server.exception_handler(RequestValidationError)
+async def handler(request: Request, exc:RequestValidationError):
+    print(exc)
+    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 if __name__=="__main__":
