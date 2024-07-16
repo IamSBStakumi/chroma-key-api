ae9f3552f308e15bf4e5b316d4220509922764e8
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2024-07-16 21:42:05 +0900

    パラメータを直接指定

--- main/variables.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# パラメータ設定
-contrast_adjustment_value = 1.5  # コントラスト調整値
-chroma_key_color = np.uint8([[[0, 255, 0]]])  # クロマキー処理の指定色（緑色）
-chroma_key_threshold = 20  # クロマキー処理の閾値
-noise_removal_iterations = 50  # ノイズ除去の繰り返し回数
