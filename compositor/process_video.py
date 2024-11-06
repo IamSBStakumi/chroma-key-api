9b1508965f9f8b2a61dff88c2f5dc5805a81be9f
kuri884 <krc.kuribayashi@gmail.com>
2024-11-06 13:32:32 +0900

    モジュール間の結合を修正

--- compositor/process_video.py
+++ compositor/process_video.py
@@ -1,18 +1,11 @@
-import gc
 import os
 from concurrent.futures import ThreadPoolExecutor
 
 import cv2
-import numpy as np
+
+from compositor import create_frame as cf
 
 executor = ThreadPoolExecutor(max_workers=os.cpu_count())
-
-# パラメータ設定
-contrast_adjustment_value = 1.5  # コントラスト調整値
-chroma_key_color = np.uint8([[[0, 255, 0]]])  # クロマキー処理の指定色（緑色）
-chroma_key_threshold = 20  # クロマキー処理の閾値
-noise_removal_iterations = 50  # ノイズ除去の繰り返し回数
-
 
 def process_video(temp_dir, image_path, video_path):
     video = cv2.VideoCapture(video_path)
@@ -32,35 +25,8 @@ def process_video(temp_dir, image_path, video_path):
 
     # フレーム処理を並行して行う
     def process_and_write_frame(i, movie_frame):
-        output_frame = create_frame(movie_frame)
+        output_frame = cf.create_frame(movie_frame, back)
         return i, output_frame
-
-    def create_frame(input_frame):
-        # コントラスト調整
-        contrast_image = cv2.convertScaleAbs(input_frame, alpha=contrast_adjustment_value, beta=0)
-
-        # クロマキー処理と二値化
-        hsv_chroma_key_color = cv2.cvtColor(chroma_key_color, cv2.COLOR_BGR2HSV)
-        lower_green = np.array([hsv_chroma_key_color[0][0][0] - chroma_key_threshold, 50, 50])
-        upper_green = np.array([hsv_chroma_key_color[0][0][0] + chroma_key_threshold, 255, 255])
-        hsv_image = cv2.cvtColor(contrast_image, cv2.COLOR_BGR2HSV)
-        chroma_key_image = cv2.inRange(hsv_image, lower_green, upper_green)
-        mask_image = cv2.bitwise_not(chroma_key_image)
-
-        # ノイズ除去
-        transparent_image = cv2.cvtColor(input_frame, cv2.COLOR_BGR2BGRA)  # RGBA形式に変換
-        transparent_image[:, :, 3] = mask_image  # アルファチャンネルにマスク画像を設定
-
-        output_frame = cv2.convertScaleAbs(
-            back * (1 - (transparent_image[:, :, 3:] / 255.0)) + transparent_image[:, :, :3] * (transparent_image[:, :, 3:] / 255.0)
-        )
-
-        # 明示的に不要なデータを開放
-        del contrast_image, hsv_image, chroma_key_image, mask_image, transparent_image
-        # ガベージコレクション実行
-        gc.collect()
-
-        return output_frame
 
     futures = []
     # bar_template = "\r[{0}] {1}/100%"
@@ -71,12 +37,12 @@ def process_video(temp_dir, image_path, video_path):
             break
 
         # frameごとの処理をsubmit
-        futures.append(executor.submit(process_and_write_frame, i, movie_frame))
+        # futures.append(executor.submit(process_and_write_frame, i, movie_frame))
 
     # すべての処理が終わるのを待つ
-    for future in futures:
-        i, output_frame = future.result()
-        # _, output_frame = process_and_write_frame(i, movie_frame)
+    # for future in futures:
+        # i, output_frame = future.result()
+        _, output_frame = process_and_write_frame(i, movie_frame)
         # フレームをエンコーダに送信
         writer.write(output_frame)
 
