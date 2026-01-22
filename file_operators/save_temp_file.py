7407394db66acb402450c4d27fa12db200f38f14
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2026-01-22 11:12:23 +0900

    読み込み可能ファイルバッファを１KBから１MBに強化

--- file_operators/save_temp_file.py
+++ file_operators/save_temp_file.py
@@ -9,7 +9,7 @@ async def save_temp_file(upload_file: UploadFile, temp_dir: str, filename: str):
     destination_path = os.path.join(temp_dir, filename)
 
     async with aiofiles.open(destination_path, "wb") as out_file:
-        while content := await upload_file.read(1024):
+        while content := await upload_file.read(1024 * 1024):
             await out_file.write(content)
     
     return destination_path
